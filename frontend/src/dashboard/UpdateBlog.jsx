f123ebdee9b1d057aac4541624fef551f3c22d21
Suvam Manna <125136722+suvam-manna@users.noreply.github.com>
2024-11-02 19:45:23 +0530

    Update UpdateBlog.jsx

--- frontend/src/dashboard/UpdateBlog.jsx
+++ frontend/src/dashboard/UpdateBlog.jsx
@@ -29,7 +29,7 @@ function UpdateBlog() {
     const fetchBlog = async () => {
       try {
         const { data } = await axios.get(
-          `http://localhost:4001/api/blogs/single-blog/${id}`,
+          `https://suvblog.onrender.com/api/blogs/single-blog/${id}`,
 
           {
             withCredentials: true,
@@ -61,7 +61,7 @@ function UpdateBlog() {
     formData.append("blogImage", blogImage);
     try {
       const { data } = await axios.put(
-        `http://localhost:4001/api/blogs/update/${id}`,
+        `https://suvblog.onrender.com/api/blogs/update/${id}`,
         formData,
         {
           withCredentials: true,
