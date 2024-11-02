f29c8ac6c497582603f19adf8b8c1357a2ad5d81
Suvam Manna <125136722+suvam-manna@users.noreply.github.com>
2024-11-02 19:47:00 +0530

    Update Creators.jsx

--- frontend/src/pages/Creators.jsx
+++ frontend/src/pages/Creators.jsx
@@ -9,7 +9,7 @@ function Creators() {
     const fetchCreators = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4001/api/users/admins",
+          "https://suvblog.onrender.com/api/users/admins",
           {
             withCredentials: true, // to pass cookie/token
           }
