d1a95ae5a13ee1617e50085087d46d563e1552f4
Suvam Manna <125136722+suvam-manna@users.noreply.github.com>
2024-11-02 19:48:01 +0530

    Update Detail.jsx

--- frontend/src/pages/Detail.jsx
+++ frontend/src/pages/Detail.jsx
@@ -12,7 +12,7 @@ function Detail() {
     const fetchblogs = async () => {
       try {
         const { data } = await axios.get(
-          `http://localhost:4001/api/blogs/single-blog/${id}`,
+          `https://suvblog.onrender.com/api/blogs/single-blog/${id}`,
 
           {
             withCredentials: true,
