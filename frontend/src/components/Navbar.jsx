7127b99a0fbb9dcf1879e87fb75c745369c5bbe6
Suvam Manna <125136722+suvam-manna@users.noreply.github.com>
2024-11-02 19:40:04 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        "http://localhost:4001/api/users/logout",
+        "https://suvblog.onrender.com/api/users/logout",
         { withCredentials: true }
       );
       console.log(data);
