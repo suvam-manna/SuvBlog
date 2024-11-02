661f5126ea83447d14abe3ee7732243d4687fb93
Suvam Manna <125136722+suvam-manna@users.noreply.github.com>
2024-11-02 19:42:51 +0530

    Update MyBlogs.jsx

--- frontend/src/dashboard/MyBlogs.jsx
+++ frontend/src/dashboard/MyBlogs.jsx
@@ -9,7 +9,7 @@ function MyBlogs() {
     const fetchMyBlogs = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4001/api/blogs/my-blog",
+          "https://suvblog.onrender.com/api/blogs/my-blog",
           { withCredentials: true }
         );
         console.log(data);
@@ -23,7 +23,7 @@ function MyBlogs() {
 
   const handleDelete = async (id) => {
     await axios
-      .delete(`http://localhost:4001/api/blogs/delete/${id}`, {
+      .delete(`https://suvblog.onrender.com/api/blogs/delete/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
